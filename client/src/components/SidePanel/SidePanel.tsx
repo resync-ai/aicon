377ae08e6c3ba0ad6ddc9f995a15e5e6db3b7c65
Rudolf Bok <rudolfbok@Rudy---MacBook-Air.local>
2025-03-07 16:12:50 +0100

    few more changes

--- client/src/components/SidePanel/SidePanel.tsx
+++ client/src/components/SidePanel/SidePanel.tsx
@@ -164,7 +164,7 @@ const SidePanel = ({
           localStorage.setItem('react-resizable-panels:collapsed', 'true');
         }}
         className={cn(
-          'sidenav hide-scrollbar border-l border-border-light bg-background py-1 transition-opacity',
+          'sidenav hide-scrollbar border-l border-border-light bg-background transition-opacity pt-2',
           isCollapsed ? 'min-w-[50px]' : 'min-w-[340px] sm:min-w-[352px]',
           (isSmallScreen && isCollapsed && (minSize === 0 || collapsedSize === 0)) || fullCollapse
             ? 'hidden min-w-0'
