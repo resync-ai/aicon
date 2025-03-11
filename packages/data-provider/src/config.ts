23ae3f00579f863ec9b78a95be4645881309700c
Rudolf Bok <rudolfbok@Rudy---MacBook-Air.local>
2025-03-11 20:47:57 +0100

    Assistants => Asistenti

--- packages/data-provider/src/config.ts
+++ packages/data-provider/src/config.ts
@@ -670,7 +670,7 @@ export const defaultEndpoints: EModelEndpoint[] = [
 
 export const alternateName = {
   [EModelEndpoint.openAI]: 'AICon',
-  [EModelEndpoint.assistants]: "Assistants",
+  [EModelEndpoint.assistants]: "Asistenti",
   [EModelEndpoint.agents]: 'Agents',
   [EModelEndpoint.azureAssistants]: 'Azure Assistants',
   [EModelEndpoint.azureOpenAI]: 'Azure OpenAI',
