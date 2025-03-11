e637c64e81a525ed7c219b5c03e3c14dcc77a321
Rudolf Bok <rudolfbok@Rudy---MacBook-Air.local>
2025-03-11 17:36:21 +0100

    login/signup button colors & settings functions done

--- client/src/components/Nav/SettingsTabs/Chat/Chat.tsx
+++ client/src/components/Nav/SettingsTabs/Chat/Chat.tsx
@@ -35,27 +35,6 @@ const toggleSwitchConfigs = [
     key: 'showThinking',
   },
   {
-    stateAtom: store.showCode,
-    localizationKey: 'com_nav_show_code',
-    switchId: 'showCode',
-    hoverCardText: undefined,
-    key: 'showCode',
-  },
-  {
-    stateAtom: store.LaTeXParsing,
-    localizationKey: 'com_nav_latex_parsing',
-    switchId: 'latexParsing',
-    hoverCardText: 'com_nav_info_latex_parsing',
-    key: 'latexParsing',
-  },
-  {
-    stateAtom: store.saveDrafts,
-    localizationKey: 'com_nav_save_drafts',
-    switchId: 'saveDrafts',
-    hoverCardText: 'com_nav_info_save_draft',
-    key: 'saveDrafts',
-  },
-  {
     stateAtom: store.showScrollButton,
     localizationKey: 'com_nav_scroll_button',
     switchId: 'showScrollButton',
@@ -68,13 +47,6 @@ const toggleSwitchConfigs = [
     switchId: 'showBadges',
     hoverCardText: 'com_nav_info_save_badges_state',
     key: 'showBadges',
-  },
-  {
-    stateAtom: store.modularChat,
-    localizationKey: 'com_nav_modular_chat',
-    switchId: 'modularChat',
-    hoverCardText: undefined,
-    key: 'modularChat',
   },
 ];
 
--- client/src/components/Nav/SettingsTabs/General/General.tsx
+++ client/src/components/Nav/SettingsTabs/General/General.tsx
@@ -12,25 +12,11 @@ import store from '~/store';
 
 const toggleSwitchConfigs = [
   {
-    stateAtom: store.enableUserMsgMarkdown,
-    localizationKey: 'com_nav_user_msg_markdown',
-    switchId: 'enableUserMsgMarkdown',
-    hoverCardText: undefined,
-    key: 'enableUserMsgMarkdown',
-  },
-  {
     stateAtom: store.autoScroll,
     localizationKey: 'com_nav_auto_scroll',
     switchId: 'autoScroll',
     hoverCardText: undefined,
     key: 'autoScroll',
-  },
-  {
-    stateAtom: store.hideSidePanel,
-    localizationKey: 'com_nav_hide_panel',
-    switchId: 'hideSidePanel',
-    hoverCardText: undefined,
-    key: 'hideSidePanel',
   },
 ];
 
